659d8d5574ce92b1487c7406aefde82c3a2d7e73
leavan <leavan.vicente@epitech.eu>
2021-01-14 15:27:04 +0100

    automatisation of writable and readable stream with pipe

---
+++ Pipes/app.js
@@ -0,0 +1,18 @@
+var http = require('http');
+var fs = require('fs');
+
+// var myReadStream = fs.createReadStream(__dirname + '/read.txt', 'utf8');
+// var myWriteStream = fs.createWriteStream(__dirname + '/writeMe.txt');
+// //change happened => has the same function as saw in writable stream
+// myReadStream.pipe(myWriteStream);
+
+
+const server = http.createServer(function(req, res){
+  console.log('request was made: ' + req.url);
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  var myReadStream = fs.createReadStream(__dirname + '/read.txt', 'utf8');
+  myReadStream.pipe(res);
+});
+
+server.listen(3000, '127.0.0.1');
+console.log("listening to port 3000");
