deec7db338cdec34fcac22155634a5e812621a61
leavan <leavan.vicente@epitech.eu>
2021-01-14 23:18:38 +0100

    basic template with ejs

---
+++ TemplatesEngines/app.js
@@ -0,0 +1,23 @@
+var express = require('express');
+var app = express();
+
+app.set('view engine', 'ejs');
+
+app.get('/', function(req, res){
+  res.render('index');
+});
+
+app.get('/contact', function(req, res){
+  res.render('contact');
+});
+
+app.get('/profile/:name', function(req, res){
+  var data = {age: 25, job: 'Dev', hobbies: ['eating', 'playing', 'reading']};
+  res.render('profile', {person: req.params.name, data: data});
+});
+
+app.use('', function(req, res){
+  res.render('404');
+})
+
+app.listen(3000);
